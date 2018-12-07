a1eeb611b26b96b0b087558a48e3b9a4b2493015
Mindey <mindey@qq.com>
2018-12-07 17:10:25 +0000

    add gun, and todo functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Gun from 'gun'
+
+const gun = Gun().get('todos')
+const formatTodos = todos => Object.keys(todos)
+  .map(key => ({ key, val: todos[key] }))
+  .filter(t => Boolean(t.val) && t.key !== '_')
+
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {newTodo: '', todos: []}
+  }
+
+  componentWillMount() {
+    gun.on(todos => this.setState({
+      todos: formatTodos(todos)
+    }))
+  }
+
+  add = _ => {
+    gun.get(Gun.text.random()).put(this.state.newTodo)
+    this.setState({newTodo: ''})
+  }
+
+  del = key => gun.get(key).put(null)
+
+  handleChange = e => this.setState({ newTodo: e.target.value})
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <input value={this.state.newTodo} onChange={this.handleChange} />
+        <button onClick={this.add}>Add</button>
+        <br />
+        <ul>
+          {this.state.todos.map(todo => <li key={todo.key} onClick={_=>this.del(todo.key)}>{todo.val}</li>)}
+        </ul>
       </div>
     );
   }
